d581b9f4dfb50e76d50c1187e578c2d57e2a6111
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-03-24 11:39:07 +0000

    destructure title of props, according to the sketch, there will be further individual props such as image_url

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./menu-item.style.scss"
 
-const MenuItem = (props) => (
+const MenuItem = ({ title }) => (
   <div className="menu-item">
     <div className="content">
-      <h1 className="title">props.title</h1>
+      <h1 className="title">{ title }</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
