0e14da73642e2df1d5aa0025996bfd0a5c3adf33
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-03-26 13:12:39 +0000

    Setup the link to hats url

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -10,27 +10,32 @@ class DirectoryMenu extends React.Component {
       sections: [{
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+          linkUrl: "hats",
           id: 1
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+          linkUrl: "",
           id: 2
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+          linkUrl: "",
           id: 3
         },
         {
           title: "womens",
           imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+          linkUrl: "",
           size: "large",
           id: 4
         },
         {
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+          linkUrl: "",
           size: "large",
           id: 5
         }
@@ -42,9 +47,10 @@ class DirectoryMenu extends React.Component {
     return (
       <div className="directory-menu">
         {
-          this.state.sections.map(({title, imageUrl, size, id}) => (
+          this.state.sections.map(({title, imageUrl, linkUrl, size, id}) => (
             <MenuItem title={ title }
                       imageUrl={ imageUrl }
+                      linkUrl={ linkUrl }
                       size={ size }
                       key={ id } />
           ))
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import "./menu-item.style.scss"
 import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size, history }) => (
-  <div className={`${ size } menu-item`}>
+const MenuItem = ({ title, imageUrl, linkUrl, size, history, match }) => (
+  <div className={ `${ size } menu-item` } onClick={ () => history.push(`${match.url}${linkUrl}`) }>
     <div className="background-image"
          style={{ backgroundImage: `url(${ imageUrl })` }}></div>
     <div className="content">
       <h1 className="title">{ title.toUpperCase() }</h1>
       <span className="subtitle">SHOP NOW</span>
-      <span>{ history.location.pathname }</span>
     </div>
   </div>
 );
