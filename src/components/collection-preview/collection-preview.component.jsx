603e255dbc1a55912254829a38178b1f8fcd31a9
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-03-26 22:42:04 +0000

    Incorporate collection-preview into shop page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./collection-preview.style.scss"
 
-const CollectionPreview = () => (
+const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
-    <h1 className="title">Title</h1>
+    <h1 className="title">{ title.toUpperCase() }</h1>
     <div className="preview">
+      {
+        items.map((item) => (
+          <div key={ item.id }>{ item.name }</div>
+        ))
+      }
     </div>
   </div>
 )
