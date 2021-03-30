d343ec5d82edb11e669ee64904994f7c730d132b
Lord of the Boxes <lordoftheboxes@localhost.localdomain>
2021-03-30 12:06:20 +0100

    Create placeholder page

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sign-in-sign-up.style.scss";
+
+const SignInSignUpPage = () => (
+  <div className="sign-in-sign-up">SIGN IN/SIGN UP</div>
+)
+
+export default SignInSignUpPage;
