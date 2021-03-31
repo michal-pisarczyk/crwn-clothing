84ef129223e9f5312c0348b857b0d002a87d9cc2
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-03-31 15:23:41 +0100

    Turn App into a state component, with the access to current logged in user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
+import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ HomePage } />
-        <Route path="/shop" component={ ShopPage } />
-        <Route path="/signinsignup" component={ SignInSignUpPage } />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={ HomePage } />
+          <Route path="/shop" component={ ShopPage } />
+          <Route path="/signinsignup" component={ SignInSignUpPage } />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App;
