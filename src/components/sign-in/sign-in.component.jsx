d6e8b99a82f8217032a929847f9c5376f6ad28dd
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-03-31 14:42:33 +0100

    Add working button for Google Sign In

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./sign-in.style.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
   constructor() {
@@ -42,6 +43,7 @@ class SignIn extends React.Component {
                      value={ this.state.password }
                      required />
           <CustomButton type="submit">Sign in</CustomButton>
+          <CustomButton onClick={ signInWithGoogle }>Sign in with Google</CustomButton>
         </form>
       </div>
     )
