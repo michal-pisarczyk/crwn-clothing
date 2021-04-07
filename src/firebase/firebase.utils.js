cc8caf6df6da136a2150282f5469983cc73b52dd
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-07 15:49:03 +0100

    Log in potential error message

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,7 +20,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-
+      console.log("Error creating user record. ", error.message);
     }
   }
 }
