d21b3f1cdf51cb6faa3c5d23d780d2be80e86c35
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-19 13:32:42 +0100

    Handle submit - overwrite the default

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -16,12 +16,45 @@ class SignUp extends React.Component {
     }
   }
 
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    const { displayName,
+            email,
+            password,
+            confirmPassword } = this.state;
+
+    if (password != confirmPassword) {
+      alert("Passwords don't match!");
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      await createUserProfileDocument(user, { displayName });
+
+      this.setState({
+        displayName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <div className="sign-up">
         <h2>I do not have an account.</h2>
         <span>Sign up with your email and password.</span>
-        <form>
+        <form onSubmit={ this.handleSubmit }>
           <FormInput label="Display Name"
                      type="text"
                      name="sign-up-name"
