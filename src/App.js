cc29b66934e2e33fde653c1c1f5b03df8e2cbae3
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-22 11:24:31 +0100

    Implement redirect on Sign In

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
@@ -41,15 +41,25 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ HomePage } />
           <Route path="/shop" component={ ShopPage } />
-          <Route path="/signinsignup" component={ SignInSignUpPage } />
+          <Route exact path="/signinsignup" render={
+            () => this.props.currentUser ?
+              (<Redirect to="/" />) :
+              (<SignInSignUpPage />) } />
         </Switch>
       </div>
     )
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
