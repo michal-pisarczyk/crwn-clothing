c533f517f9c8e35cf3486d8107496158773047b9
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-27 12:06:52 +0100

    Create redux selector for quantity of cart items in cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,26 @@ import { ReactComponent as CartRepresentation } from "../../assets/shopping-bag.
 import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={ toggleCartHidden }>
     <CartRepresentation className="cart-representation" />
-    <span className="item-count">0</span>
+    <span className="item-count">{ itemCount }</span>
   </div>
 )
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+// redux selector
+//   slice of the state
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accQuantity, cartItem) => accQuantity + cartItem.quantity, 0
+  )
+});
+// the performance issue here is, so that whenever state changes,
+// the code above always triggers regardless of the fact if number
+// of the cart items changed or not
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
