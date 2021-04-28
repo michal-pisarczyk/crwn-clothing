d3c35a03e9a6b65a0275820aefcd23e027d57700
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-28 17:57:45 +0100

    Toggle cart hidden on click of 'Go to checkout' button via dispatch

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,30 +7,31 @@ import { selectCartItems } from "../../redux/cart/cart.selectors";
 import { withRouter } from "react-router-dom";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 
-const CartDropdown = ({ cartItems, history, ...otherProps }) => {
-  console.log(otherProps);
-
-  return (
-    <div className="cart-dropdown">
-      <div className="cart-items">
-        {
-          cartItems.length ?
-          cartItems.map(
-            cartItem => <CartItem key={ cartItem.id }
-                                  item={ cartItem } />
-          ) :
-          <span className="empty-cart-message">
-            Your cart is empty.
-          </span>
-        }
-      </div>
-      <CustomButton
-        onClick={ () => history.push("/checkout") }>
-        Go to checkout
-      </CustomButton>
+const CartDropdown = ({ cartItems, history, dispatch }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {
+        cartItems.length ?
+        cartItems.map(
+          cartItem => <CartItem key={ cartItem.id }
+                                item={ cartItem } />
+        ) :
+        <span className="empty-cart-message">
+          Your cart is empty.
+        </span>
+      }
     </div>
-  )
-}
+    <CustomButton
+      onClick={
+        () => {
+          history.push("/checkout")
+          dispatch(toggleCartHidden())
+        }
+      }>
+      Go to checkout
+    </CustomButton>
+  </div>
+);
 
 const mapStateToProps = state => ({
   cartItems: selectCartItems(state)
