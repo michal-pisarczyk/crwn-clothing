064a17c7f950225c3d7162523c412b1d27baa8cc
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-30 14:29:01 +0100

    Fix typo

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./root-reducer";
 
-const midlewares = [logger];
+const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...midlewares));
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
