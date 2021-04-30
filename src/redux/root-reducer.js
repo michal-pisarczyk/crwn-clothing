9e25e458ff0c5e864178fb4b86ed5f0f2b133940
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-04-30 18:55:00 +0100

    Include directory menu reducer in combined root reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryMenuReducer from "./directory-menu/directory-menu.reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // local storage - not session storage
 
@@ -12,7 +13,8 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directoryMenu: directoryMenuReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
