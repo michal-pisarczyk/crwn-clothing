03b5cf05f42515d14da48975252fc84c45f99b80
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-05-04 12:12:33 +0100

    Destructure title and items off of collection object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./collection.style.scss";
-// import CollectionItem from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import { connect } from "react-redux";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({ collection }) => {
-  console.log(collection);
+  const { title, items } = collection;
 
   return (
     <div className="collection-page">
-      <h2>COLLECTION PAGE</h2>
+      <h2>{ title }</h2>
+      <div>
+        {
+          items.map(
+            item => <CollectionItem key={ item.id } item={ item } />
+          )
+        }
+      </div>
     </div>
   );
 };
