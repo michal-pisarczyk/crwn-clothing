b5d3e91c480994503e1f485248a7fd4273553e26
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-05-04 13:39:52 +0100

    Make adjustments to the linkUrl in directory menu reducer, refactor directory menu and menu item components slightly

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./directory-menu.style.scss";
 import MenuItem from "../menu-item/menu-item.component";
-import { selectDirectoryMenuSections } from "../../redux/directory-menu/directory-menu.selectors";
+import { selectDirectoryMenuSections }
+  from "../../redux/directory-menu/directory-menu.selectors";
 import { connect } from "react-redux";
 
 const DirectoryMenu = ({ sections }) => (
   <div className="directory-menu">
     {
-      sections.map(({ id, ...otherSectionProps }) => (
-        <MenuItem key={ id } { ...otherSectionProps } />
-      ))
+      sections.map(
+        ({ id, ...otherSectionProps }) =>
+          <MenuItem key={ id } { ...otherSectionProps } />
+      )
     }
   </div>
 );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import "./menu-item.style.scss"
 import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, linkUrl, size, history, match }) => (
-  <div className={ `${ size } menu-item` } onClick={ () => history.push(`${match.url}${linkUrl}`) }>
-    <div className="background-image"
-         style={{ backgroundImage: `url(${ imageUrl })` }}></div>
-    <div className="content">
-      <h1 className="title">{ title.toUpperCase() }</h1>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = sectionProps => {
+  const { title, imageUrl, linkUrl, size,
+          history, match } = sectionProps;
+  const subtitle = "Shop Now";
+
+  return (
+    <div className={ `${ size } menu-item` }
+         onClick={ () => history.push(`${ match.url }${ linkUrl }`) }>
+      <div className="background-image"
+           style={{ backgroundImage: `url(${ imageUrl })` }}></div>
+      <div className="content">
+        <h1 className="title">{ title.toUpperCase() }</h1>
+        <span className="subtitle">{ subtitle.toUpperCase() }</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default withRouter(MenuItem);
