b5d7afb969962a704919ce60d3d52a9d4d7f7fdd
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-05-11 17:24:25 +0100

    Turn directory menu into styled component

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -1,19 +1,18 @@
 import React from "react";
-import "./directory-menu.style.scss";
+import { MenuContainer } from "./directory-menu.style";
 import MenuItem from "../menu-item/menu-item.component";
 import { selectDirectoryMenuSections }
   from "../../redux/directory-menu/directory-menu.selectors";
 import { connect } from "react-redux";
 
 const DirectoryMenu = ({ sections }) => (
-  <div className="directory-menu">
+  <MenuContainer>
     {
-      sections.map(
-        ({ id, ...otherSectionProps }) =>
-          <MenuItem key={ id } { ...otherSectionProps } />
+      sections.map(({ id, ...otherSectionProps }) =>
+        <MenuItem key={ id } { ...otherSectionProps } />
       )
     }
-  </div>
+  </MenuContainer>
 );
 
 const mapStateToProps = state => ({
