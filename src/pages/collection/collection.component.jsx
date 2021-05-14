a15d5818a5b123047592a10499b4e29a5b4d7e6f
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-05-14 10:51:32 +0100

    Add styled component for collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,23 +1,25 @@
 import React from "react";
-import "./collection.style.scss";
+import { CollectionPageContainer, Title, Items }
+  from "./collection.style";
+import CollectionItem
+  from "../../components/collection-item/collection-item.component";
 import { selectCollection } from "../../redux/shop/shop.selectors";
 import { connect } from "react-redux";
-import CollectionItem from "../../components/collection-item/collection-item.component";
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
 
   return (
-    <div className="collection-page">
-      <h2 className="title">{ title }</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <Title>{ title }</Title>
+      <Items>
         {
           items.map(
             item => <CollectionItem key={ item.id } item={ item } />
           )
         }
-      </div>
-    </div>
+      </Items>
+    </CollectionPageContainer>
   );
 };
 
