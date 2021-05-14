9d7f7cf66ae4fd49ea685faeeddde6886a1e7019
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-05-14 11:00:31 +0100

    Turn shop page component into a styled component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { ShopPageContainer } from "./shop.style";
 import { Route } from "react-router-dom";
 import CollectionsOverview from
 "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
 const ShopPage = ({ match }) => (
-  <div className="shop-page">
+  <ShopPageContainer>
     <Route
       exact path={ `${match.path}` }
       component={ CollectionsOverview } />
     <Route
       path={ `${match.path}/:collectionId` }
       component={ CollectionPage } />
-  </div>
+  </ShopPageContainer>
 );
 
 export default ShopPage;
