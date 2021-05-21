dd4341a7ac6f841ec77f26556ccf7070457e8448
Michał Pisarczyk <michpisarczyk@gmail.com>
2021-05-21 11:42:23 +0100

    Simplify code

--- src/components/with-spinner/with-spinner.component.jsx
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { SpinnerOverlay, SpinnerContainer }
   from "./with-spinner.style";
 
-const WithSpinner = WrappedComponent => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
-    return isLoading ?
-    <SpinnerOverlay><SpinnerContainer /></SpinnerOverlay> :
-    <WrappedComponent { ...otherProps } />
-  };
-  return Spinner;
+const WithSpinner = WrappedComponent =>
+                    ({ isLoading, ...otherProps }) => {
+  return isLoading ?
+  <SpinnerOverlay><SpinnerContainer /></SpinnerOverlay> :
+  <WrappedComponent { ...otherProps } />
 };
 
 export default WithSpinner;
