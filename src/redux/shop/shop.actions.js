548e85ac8ba6eb988a7334952e2aa655208b3807
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-05-26 11:00:49 +0100

    Move async portion of code from shop component into redux async action

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,45 @@
 import ShopActionTypes from "./shop.types";
+import { firestore } from "../../firebase/firebase.utils";
 
 export const updateCollections = collectionsMap => ({
   type: ShopActionTypes.UPDATE_COLLECTIONS,
   payload: collectionsMap
 });
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionReference = firestore.collection("collections");
+
+    const convertSnapshotToMap = collections => {
+      const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+          id: doc.id,
+          routeName: encodeURI(title.toLowerCase()),
+          title,
+          items
+        };
+      });
+
+      return transformedCollection.reduce(
+        (accumulator, collection) => {
+          accumulator[collection.title.toLowerCase()] = collection;
+          return accumulator;
+        }, {}
+      );
+    };
+
+    collectionReference
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertSnapshotToMap(snapshot);
+        updateCollections(collectionsMap);
+        this.setState({ loading: false });
+      });
+  };
+};
