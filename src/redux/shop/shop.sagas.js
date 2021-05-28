42f6674d025f941d145061e5990aeb1ff71592b0
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-05-28 14:07:14 +0100

    Add shop sagas file

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,45 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+import ShopActionTypes from "./shop.types";
+import { firestore } from "../../firebase/firebase.utils";
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from "./shop.actions";
+
+const convertSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      id: doc.id,
+      routeName: encodeURI(title.toLowerCase()),
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce(
+    (accumulator, collection) => {
+      accumulator[collection.title.toLowerCase()] = collection;
+      return accumulator;
+    }, {}
+  );
+};
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionReference = firestore.collection("collections");
+    const snapshot = yield collectionReference.get();
+    const collectionsMap = yield call(convertSnapshotToMap, snapshot);
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
