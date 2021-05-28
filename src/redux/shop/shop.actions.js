f37bae70e4ed36c0e2a2bc8ea19d419db07cc820
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-05-28 14:08:19 +0100

    Modify shop actions

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -14,38 +14,3 @@ export const fetchCollectionsFailure = errorMessage => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
   payload: errorMessage
 });
-
-export const fetchCollectionsStartAsync = () => {
-  return dispatch => {
-    const collectionReference = firestore.collection("collections");
-    dispatch(fetchCollectionsStart());
-
-    const convertSnapshotToMap = collections => {
-      const transformedCollection = collections.docs.map(doc => {
-        const { title, items } = doc.data();
-
-        return {
-          id: doc.id,
-          routeName: encodeURI(title.toLowerCase()),
-          title,
-          items
-        };
-      });
-
-      return transformedCollection.reduce(
-        (accumulator, collection) => {
-          accumulator[collection.title.toLowerCase()] = collection;
-          return accumulator;
-        }, {}
-      );
-    };
-
-    collectionReference
-      .get()
-      .then(snapshot => {
-        const collectionsMap = convertSnapshotToMap(snapshot);
-        dispatch(fetchCollectionsSuccess(collectionsMap));
-        // this.setState({ loading: false });
-      }).catch(error => dispatch(fetchCollectionsFailure(error.message)));
-  };
-};
