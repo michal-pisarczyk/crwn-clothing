f6453065e326c436b9c5d4ed00e3c95ec94c068d
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-06-08 10:21:09 +0100

    Covert sign in component logic into saga

--- src/App.js
+++ src/App.js
@@ -6,31 +6,11 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { connect } from "react-redux";
-import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { setCurrentUser } = this.props;
-
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-
-        userRef.onSnapshot(snapshot => {
-          setCurrentUser({
-            id: snapshot.id,
-            ...snapshot.data()
-          });
-        });
-      }
-      setCurrentUser(userAuth);
-    });
-  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -58,8 +38,4 @@ const mapStateToProps = state => ({
   currentUser: selectCurrentUser(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { SignInContainer, ButtonsContainer } from "./sign-in.style";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { auth, signInWithGoogle }
-  from "../../firebase/firebase.utils";
+import { googleSignInStart, emailSignInStart }
+  from "../../redux/user/user.actions";
+import { connect } from "react-redux";
 
 class SignIn extends React.Component {
   constructor() {
@@ -17,21 +18,9 @@ class SignIn extends React.Component {
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const { emailSignInStart } = this.props;
     const { email, password } = this.state;
-
-    try {
-      await auth.signInWithEmailAndPassword(
-        email,
-        password
-      );
-      // on success, clear state.email and
-      //                   state.password
-      this.setState({ email: "", password: "" });
-
-    } catch (error) {
-      console.error(error);
-    }
+    emailSignInStart(email, password);
   }
 
   handleChange = event => {
@@ -40,6 +29,7 @@ class SignIn extends React.Component {
   }
 
   render() {
+    const { googleSignInStart } = this.props;
     return (
       <SignInContainer>
         <h2>I already have an account.</h2>
@@ -60,7 +50,7 @@ class SignIn extends React.Component {
           <ButtonsContainer>
             <CustomButton type="submit">Sign in</CustomButton>
             <CustomButton type="button"
-                          onClick={ signInWithGoogle }
+                          onClick={ googleSignInStart }
                           isGoogleSignIn>
               Sign in with Google
             </CustomButton>
@@ -71,4 +61,11 @@ class SignIn extends React.Component {
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) => dispatch(
+    emailSignInStart({ email, password })
+  )
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
