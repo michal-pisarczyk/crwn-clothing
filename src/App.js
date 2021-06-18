b82afe036a192e6f32a82c1689bb2cac1761d19f
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-06-18 10:49:54 +0100

    Convert App component with useEffect hook in place of the class component with componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import SignInSignUpPage
+  from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import { connect } from "react-redux";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const { checkUserSession } = this.props;
+const App = ({ checkUserSession, currentUser }) => {
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={ HomePage } />
-          <Route path="/shop" component={ ShopPage } />
-          <Route exact path="/checkout" component={ CheckoutPage } />
-          <Route exact path="/signinsignup" render={
-            () => this.props.currentUser ?
-              (<Redirect to="/" />) :
-              (<SignInSignUpPage />) } />
-        </Switch>
-      </div>
-    )
-  }
-}
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route path="/shop" component={ ShopPage } />
+        <Route exact path="/checkout" component={ CheckoutPage } />
+        <Route exact path="/signinsignup" render={
+          () => currentUser ?
+            (<Redirect to="/" />) :
+            (<SignInSignUpPage />) } />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = state => ({
   currentUser: selectCurrentUser(state)
