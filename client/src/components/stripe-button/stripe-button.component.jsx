84e0146b8d7193dea8dc2865e83fbe71ec4097b3
Michal Pisarczyk <michpisarczyk@gmail.com>
2021-06-24 11:11:20 +0100

    Build a simple express payment server for stripe payments

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_51Io6fKGWJKdMC2S6eohsol1jHK4wsIMKJJDyYESHJfn7YsT8GFruGQnZ3tWz0i1NYsi5zA3gPSAqpzVe9r6N79Qn009FNz8tHK";
 
   const onToken = token => {
-    console.log(token);
-    alert("Payment was successful!");
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert("Payment was successful.");
+    }).catch(error => {
+      console.error(`Payment error: ${ JSON.parse(error) }`);
+      alert("There was an issue with your payment. Please make sure to use the provided credit card.");
+    });
   };
 
   return (
